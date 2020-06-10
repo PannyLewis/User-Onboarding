3fe1d0e4cb623385ee27097068b9ba3f1c2cc016
Panny Lewis <pannylewis@gmail.com>
2020-06-09 21:12:56 -0500

    Form.js:  added HTML inputs.

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -6,6 +6,7 @@ import Form from "./Form";
 function App() {
   return (
     <div className="App">
+      <h1>User Onboarding Form</h1>
       <Form />
     </div>
   );
--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import * as yup from "yup";
 
 function Form() {
-  return <div>the present is a gift</div>;
+  //   declare states and initialize to an object
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  return (
+    <form>
+      <label>
+        Name
+        <input type="text" name="name" />
+      </label>
+      <label>
+        Email
+        <input type="text" name="email" />
+      </label>
+      <label>
+        Password
+        <input type="text" name="password" />
+      </label>
+      <label>
+        <input type="checkbox" name="terms of service" />
+        Terms and Conditions
+      </label>
+      <button>Submit</button>
+    </form>
+  );
 }
 
 export default Form;
