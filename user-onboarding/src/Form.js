2f7fd5c7c451175e0f3043338cc2b55bd786c3f2
Panny Lewis <pannylewis@gmail.com>
2020-06-11 22:10:51 -0500

    Form.js:  render error display message when user enters wrong input

--- user-onboarding/src/Form.js
+++ user-onboarding/src/Form.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import * as yup from "yup";
 
-// yup help with formatting
+// yup help with validation
 const formSchema = yup.object().shape({
   name: yup.string().required("Name is a required field."),
   email: yup.string().email().required("Must be a valid email address."),
@@ -28,7 +28,7 @@ function Form() {
     terms: "",
   });
 
-  // this test and catches error on the yup formattor
+  // this test and catches error on the yup formattor.  Similar to axios promises
   const validateChange = (e) => {
     // Reach will allow us to "reach" into the schema and test only one part.
     yup
@@ -41,6 +41,7 @@ function Form() {
         });
       })
       .catch((err) => {
+        console.log(err.errors);
         setErrors({
           ...errors,
           [e.target.name]: err.errors[0],
@@ -51,13 +52,15 @@ function Form() {
   // this makes it so that whatever the user types in will be saved as values:
   const inputChanges = (e) => {
     // console.log("show input", e.target.value);
-
+    e.persist();
     let value =
       e.target.type === "checkbox" ? e.target.checkcked : e.target.value;
     setFormState({
       ...formState,
       [e.target.name]: value,
     }); /* ... the ellipses are called spread operator, they copy over data */
+
+    validateChange(e);
   };
 
   //   this works the submit button:
@@ -82,6 +85,7 @@ function Form() {
           value={formState.name}
           onChange={inputChanges}
         />
+        {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
       </label>
       <label>
         Email
@@ -92,6 +96,9 @@ function Form() {
           value={formState.email}
           onChange={inputChanges}
         />
+        {errors.email.length > 0 ? (
+          <p className="error">{errors.email}</p>
+        ) : null}
       </label>
       <label>
         Password
@@ -102,6 +109,9 @@ function Form() {
           value={formState.password}
           onChange={inputChanges}
         />
+        {errors.password.length > 0 ? (
+          <p className="error">{errors.password}</p>
+        ) : null}
       </label>
       <label>
         <input
